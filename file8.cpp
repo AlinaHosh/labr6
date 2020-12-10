9240c5809bbca960b1e7f319535bb1f7af22b45d
Alina <alinysikigorevna@gmail.com>
2020-12-10 15:21:37 +0200

    changed

---
+++ file8.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x, y;
+	cout << "X:";
+	cin >> x;
+	y = pow(x, 5);
+	cout << "y="<<y;
+}
---
+++ new.cpp
@@ -0,0 +1,7 @@
+#include "windows.h"
+#include <iostream>
+using namespace std;
+
+int x;
+cout << "x:";
+cin >> x;
