7a28f1fc3b1448a577a54d8aa09d6f5a20e8e8b8
Alina <alinysikigorevna@gmail.com>
2020-12-10 16:08:14 +0200

    deleted

--- Laboratorna6.cpp
+++ Laboratorna6.cpp
@@ -8,6 +8,6 @@ int main()
     double x, b;
     printf("Input x:");
     scanf_s("%lf", &x);
-    b = fabs(1 + x) + pow(x, 3) / 2 + pow(pow(x, 3) / 6, 1 / 4) + exp(4) / sqrt(24 + x);
+    b = fabs(1 + x) + pow(x, 3) / 2 + pow(pow(x, 3) / 6, 1 / 2) + exp(4) / sqrt(29 + x);
     printf("b=%lf", b);
 }
